5a58a211b2eeeb81ab7bfccb2eb4ff10caa6ce76
Kaakinen Valtteri <Valtteri.Kaakinen@businessoulu.com>
2023-02-23 13:56:19 +0200

    testing ui and video playback

--- test/Scripts/UI/Video_Menu_Button.gd
+++ test/Scripts/UI/Video_Menu_Button.gd
@@ -5,7 +5,8 @@ extends Button
 # var a = 2
 # var b = "text"
 export (String) var label_str
-
+export (PackedScene) var videoplayer = preload("res://test/Scenes/UI/VideoPlayer_test_scene_1.tscn")
+var videoplayer_scene
 onready var label = $Label
 onready var pos_x_orig=label.rect_position.x
 onready var scale_orig=label.rect_scale
@@ -21,14 +22,28 @@ func _ready():
 
 
 func _on_Button2_focus_entered():
+	play_video()
 	label.rect_position.x +=10
 	label.rect_pivot_offset=rect_scale/2
 	label.rect_scale+=Vector2(0.05,0.05)
 	
-
-
-
 func _on_Button2_focus_exited():
 	label.rect_position.x = pos_x_orig
 	label.rect_scale = scale_orig
 	label.rect_pivot_offset=Vector2(0,0)
+	if videoplayer_scene!=null:
+		videoplayer_scene.queue_free()
+
+func play_video():
+	create_video_panel()
+	
+func create_video_panel():
+	videoplayer_scene=videoplayer.instance()
+	add_child(videoplayer_scene)
+	#$self/Panel.rect_size_x=4
+	#print_debug($self.get_children())
+	#print_debug(get_children())
+	#print_debug($Panel.get_children())
+	#$Container.rect_size_x=1
+	
+	pass
