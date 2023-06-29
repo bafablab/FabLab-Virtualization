87f78b3f3badf78989dac12f956a91235218463e
onnirant <onnirantila@gmail.com>
2023-06-29 08:30:58 +0300

    Item 3D view now zooms with mouse wheel, set max and min zoom levels for objects

--- _Game/Scripts/scr_item_3d_view.gd
+++ _Game/Scripts/scr_item_3d_view.gd
@@ -4,6 +4,8 @@ onready var item = $Viewport/Spatial/Item
 onready var mesh_instance = $Viewport/Spatial/Item/MeshInstance
 onready var camera = $Viewport/Spatial/Camera
 var mouse_sensitivity = 1
+var min_distance = 0.09 # Minimum camera distance from object
+var max_distance = 1 # and maximum
 
 func _ready():
 	pass
@@ -11,7 +13,7 @@ func _ready():
 func init(mesh):
 	mesh_instance.mesh = mesh.mesh
 	item.transform.basis = Basis()
-	camera.translation = Vector3(0,0,2.9)
+	camera.translation = Vector3(0,0,0.3)
 
 func _input(event):
 	if(is_visible_in_tree()):
@@ -20,4 +22,15 @@ func _input(event):
 				item.rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
 				item.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
 			elif Input.is_action_pressed("right_mouse_click"):
-				camera.transform.origin.z += event.relative.y * mouse_sensitivity * 0.1
+				camera.transform.origin.z += event.relative.y * mouse_sensitivity * 0.005
+		elif event is InputEventMouseButton:
+			if Input.is_action_pressed("mouse_wheel_up"):
+				camera.transform.origin.z -= 0.1
+			elif Input.is_action_pressed("mouse_wheel_down"):
+				camera.transform.origin.z += 0.1
+		
+		# Prevent zooming too far or too close
+		if camera.transform.origin.z < min_distance:
+			camera.transform.origin.z = min_distance
+		elif camera.transform.origin.z > max_distance:
+			camera.transform.origin.z = max_distance
