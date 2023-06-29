537fcb44f141340886cdeec986594a64e56d32ce
onnirant <onnirantila@gmail.com>
2023-06-29 09:32:58 +0300

    Camera can be moved in item 3d view with middle mouse button

--- _Game/Scripts/scr_item_3d_view.gd
+++ _Game/Scripts/scr_item_3d_view.gd
@@ -3,9 +3,11 @@ extends ViewportContainer
 onready var item = $Viewport/Spatial/Item
 onready var mesh_instance = $Viewport/Spatial/Item/MeshInstance
 onready var camera = $Viewport/Spatial/Camera
-var mouse_sensitivity = 1
-var min_distance = 0.09 # Minimum camera distance from object
-var max_distance = 1 # and maximum
+export var mouse_sensitivity = 1
+var min_camera_distance = 0.09 # Minimum camera distance from object
+var max_camera_distance = 0.5 # and maximum
+var max_camera_x = 0.25 # Limits to camera movement
+var max_camera_y = 0.25
 
 func _ready():
 	pass
@@ -23,6 +25,9 @@ func _input(event):
 				item.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
 			elif Input.is_action_pressed("right_mouse_click"):
 				camera.transform.origin.z += event.relative.y * mouse_sensitivity * 0.005
+			elif Input.is_action_pressed("middle_mouse_click"):
+				camera.transform.origin.y += event.relative.y * mouse_sensitivity * 0.003
+				camera.transform.origin.x += -event.relative.x * mouse_sensitivity * 0.003
 		elif event is InputEventMouseButton:
 			if Input.is_action_pressed("mouse_wheel_up"):
 				camera.transform.origin.z -= 0.1
@@ -30,7 +35,19 @@ func _input(event):
 				camera.transform.origin.z += 0.1
 		
 		# Prevent zooming too far or too close
-		if camera.transform.origin.z < min_distance:
-			camera.transform.origin.z = min_distance
-		elif camera.transform.origin.z > max_distance:
-			camera.transform.origin.z = max_distance
+		if camera.transform.origin.z < min_camera_distance:
+			camera.transform.origin.z = min_camera_distance
+		elif camera.transform.origin.z > max_camera_distance:
+			camera.transform.origin.z = max_camera_distance
+			
+		if camera.transform.origin.x < -max_camera_x:
+			camera.transform.origin.x = -max_camera_x
+		elif camera.transform.origin.x > max_camera_x:
+			camera.transform.origin.x = max_camera_x
+			
+		if camera.transform.origin.y < -max_camera_y:
+			camera.transform.origin.y = -max_camera_y
+		elif camera.transform.origin.y > max_camera_y:
+			camera.transform.origin.y = max_camera_y
+		
+		
