3b7ed2faac38a9d246aa94b0ad18892433540da1
onnirant <onnirantila@gmail.com>
2023-07-06 12:10:12 +0300

    Simple background for tooltips

--- _Game/Scripts/scr_Crosshair.gd
+++ _Game/Scripts/scr_Crosshair.gd
@@ -1,15 +1,23 @@
 extends Control
 
+onready var tooltip = $TooltipBackground/Tooltip
+onready var tooltipbg = $TooltipBackground
+
 # Called when the node enters the scene tree for the first time.
 func _physics_process(_delta):
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		self.visible = true
 	else:
 		self.visible = false
+	
+	if tooltip.text:
+		tooltipbg.show()
+	else:
+		tooltipbg.hide()
 		
 func show_tooltip(text):
-	$Tooltip.set_text(text)
+	tooltip.set_text(text)
 	
 func clear_tooltip():
-	if $Tooltip.text:
-		$Tooltip.set_text("")
+	if tooltip.text:
+		tooltip.set_text("")
